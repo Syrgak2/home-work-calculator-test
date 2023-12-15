539214cdaf1b61d7679499799032a097b8c0d3e4
Syrgak <Kary6ekov.s@gmail.com>
2023-12-15 11:16:46 +0100

    Project created

---
+++ src/main/java/com/examples/skyPro/calculator/SkyProCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.examples.skyPro.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examples/skyPro/calculator/SkyProCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.skyPro.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
